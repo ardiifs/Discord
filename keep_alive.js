136d16cc366d9c787359185114d7ac7e9e465cc1
ardiifs <ardiifs@gmail.com>
2026-09-16 02:49:41 +0700

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,2 +1,7 @@
 const http = require('http');
-http.createServer((req, res) => res.end('alive')).listen(8000);
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Bot is alive!');
+}).listen(8000, () => {
+  console.log('Keep-alive server running on port 8000');
+});
